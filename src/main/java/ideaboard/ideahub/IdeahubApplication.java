ec9aad44db5bb4e3ab8a46b9afe49764a33daf9b
hoan0418 <hoan0418@naver.com>
2023-04-11 21:36:17 +0900

    Initial commit

---
+++ src/main/java/ideaboard/ideahub/IdeahubApplication.java
@@ -0,0 +1,13 @@
+package ideaboard.ideahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeahubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeahubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ideaboard/ideahub/IdeahubApplicationTests.java
@@ -0,0 +1,13 @@
+package ideaboard.ideahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeahubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
