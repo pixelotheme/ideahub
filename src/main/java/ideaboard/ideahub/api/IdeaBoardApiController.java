0203dc62454bace191b1ba91cf0204740928cd16
hoan0418 <hoan0418@naver.com>
2023-04-26 23:28:25 +0900

    apicontroller 소스 정리, 화면 include 설정

--- src/main/java/ideaboard/ideahub/api/IdeaBoardApiController.java
+++ src/main/java/ideaboard/ideahub/api/IdeaBoardApiController.java
@@ -17,35 +17,7 @@ public class IdeaBoardApiController {
 
     private final IdeaBoardService ideaBoardService;
 
-    @GetMapping("/api/ideaboard/getlist")
-    public List<IdeaBoardDto> getList(){
-        List<IdeaBoard> ideaBoards = ideaBoardService.findIdeaBoards();
 
-        List<IdeaBoardDto> collect = ideaBoards.stream().map(o -> IdeaBoardDto
-                        .builder()
-                        .title(o.getTitle())
-                        .content(o.getContent())
-                        .writeDate(o.getWriteDate())
-                        .updateDate(o.getUpdateDate())
-                        .build())
-                .collect(Collectors.toList());
-
-        return collect;
-    }
-
-    @GetMapping("/api/ideaboard/get/{id}")
-    public IdeaBoardDto get(@PathVariable("id") Long id){
-        IdeaBoard ideaBoard = ideaBoardService.findOne(id);
-        IdeaBoardDto ideaBoardDto = IdeaBoardDto
-                .builder()
-                .id(id)
-                .title(ideaBoard.getTitle())
-                .content(ideaBoard.getContent())
-                .writeDate(ideaBoard.getWriteDate())
-                .updateDate(ideaBoard.getUpdateDate())
-                .build();
-        return ideaBoardDto;
-    }
     @PostMapping("/api/ideaboard/post")
     public Long createBoard(@RequestBody IdeaBoardDto ideaBoardDto){
         return ideaBoardService.createBoard(ideaBoardDto.toEntity());
