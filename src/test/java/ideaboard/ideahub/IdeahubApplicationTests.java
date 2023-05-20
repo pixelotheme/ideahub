ddd541e153dd86f1c30fa13b44f264a4340c1e8a
hoan0418 <hoan0418@naver.com>
2023-05-20 19:45:15 +0900

    1. 테스트 주석

--- src/test/java/ideaboard/ideahub/IdeahubApplicationTests.java
+++ src/test/java/ideaboard/ideahub/IdeahubApplicationTests.java
@@ -3,11 +3,11 @@ package ideaboard.ideahub;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class IdeahubApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
