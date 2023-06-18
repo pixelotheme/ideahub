7852fc2b399d541c1063aaeac73657c35f797695
hoan0418 <hoan0418@naver.com>
2023-06-18 17:47:03 +0900

    1. profiles 대신 파일명으로 선택 port가 안먹는다

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -9,11 +9,11 @@
 
 #!/usr/bin/env bash
 
-# 쉬고 있는 profile 찾기: real1이 사용 중이면 real2가 쉬고 있고 반대면 real1이 쉬고 있음
+# 쉬고 있는 profile 찾기: real2가 사용 중이면 real3가 쉬고 있고 반대면 real2이 쉬고 있음
 function find_idle_profile(){
 #  현재 엔진엑스가 바라보고 있는 스프링 부트가 정상적으로 수행 중인지 ㅗ학인
 #  응답값을 HttpSatus로 받는다
-#  정상 : 200, 오류 : 400~503 사이 발생시 오류, 모두 예외로 보고 real2를 현재 profile로 사용
+#  정상 : 200, 오류 : 400~503 사이 발생시 오류, 모두 예외로 보고 real3를 현재 profile로 사용
   RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
   if [ ${RESPONSE_CODE} -ge 400 ]
